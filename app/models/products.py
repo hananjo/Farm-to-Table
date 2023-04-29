03044c356c0cd102550f5c624ba1152ed89d965c
Hanan Jomaa <hanan91jomaa@gmail.com>
2023-04-29 10:51:13 -0700

    products model

--- app/models/products.py
+++ app/models/products.py
@@ -5,4 +5,9 @@ db = SQLAlchemy()
 class Products(db.Model):
     __tablename__ = 'products'
 
-    id = db.Column(db.Integer, )
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    description = db.Column(db.String(50))
+    price = db.Column(db.Float, nullable=False)
+    rating = db.Column(db.Integer)
+    ownerId = db.Column(db.Integer, ForeignKey('usesrs.id'))
