d23cdc6055ede1d4b0f9400b102b62b6e63b53df
Laila <lailadenman@gmail.com>
2023-04-29 15:27:40 -0700

    minor fixes

--- app/models/cart_item.py
+++ app/models/cart_item.py
@@ -10,6 +10,6 @@ Base = declarative_base()
 cart_items = Table(
     "cart_items",
     Base.metadata,
-    db.Column("productId", ForeignKey("products.id"), primary_key=True),
-    db.Column("userId", ForeignKey("users.id"), primary_key=True)
+    db.Column("product_id", ForeignKey("products.id"), primary_key=True),
+    db.Column("user_id", ForeignKey("users.id"), primary_key=True)
 )
