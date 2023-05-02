7cc9c35249e950b09dfb635eb3a12dbfc56a29b4
david <david.chaotic.chu@gmail.com>
2023-05-01 23:33:17 -0400

    ran products,images,reviews table migrations

---
+++ alembic/versions/20230501_233017_create_products_table.py
@@ -0,0 +1,24 @@
+"""create products table
+
+Revision ID: 6d15f162d3c1
+Revises: 
+Create Date: 2023-05-01 23:30:17.674447
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6d15f162d3c1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
