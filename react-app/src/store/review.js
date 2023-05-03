478ceb909825886a4503d18d103733ab76a5e3d5
david <david.chaotic.chu@gmail.com>
2023-05-02 23:03:17 -0400

    finish review js store

---
+++ react-app/src/store/review.js
@@ -0,0 +1,30 @@
+// ACTION TYPE
+const LOAD_REVIEWS = 'review/LOAD_REVIEWS'
+
+
+// ACTION CREATOR
+const load = reviews => ({
+    type: LOAD_REVIEWS,
+    reviews
+})
+
+
+// THUNK
+export const loadReviews = () => async dispatch => {
+    let res = await fetch('/api/reviews')
+    if(res.ok) {
+        res = await res.json()
+        dispatch(load(res))
+    }
+}
+
+// STATE
+const initialState = {}
+const reviews = (state=initialState, action) => {
+    switch(action.type) {
+        case LOAD_REVIEWS:
+            return {...state, ...action.reviews}
+        default:
+            return state
+    }
+}
