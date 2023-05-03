d36cc414de23c69ade5c8e714ceddf104ed056f5
david <david.chaotic.chu@gmail.com>
2023-05-03 00:14:03 -0400

    added add/load/update/delete actions/creators/thunks/reducer

--- react-app/src/store/review.js
+++ react-app/src/store/review.js
@@ -1,22 +1,93 @@
-// ACTION TYPE
+// ACTION TYPES
 const LOAD_REVIEWS = 'review/LOAD_REVIEWS'
+const ADD_REVIEW = 'review/ADD_REVIEW'
+const EDIT_REVIEW = 'review/EDIT_REVIEW'
+const REMOVE_REVIEW = 'review/REMOVE_REVIEW'
 
+// ACTION CREATORS
 
-// ACTION CREATOR
-const load = reviews => ({
+// Load Reviews Action
+export const load = reviews => ({
     type: LOAD_REVIEWS,
     reviews
 })
 
+// Add Review Action
+export const addReview = (review) => ({
+    type: ADD_REVIEW,
+    review
+})
 
-// THUNK
+// Edit Review Action
+export const editReview = (review) => ({
+    type: EDIT_REVIEW,
+    review
+})
+
+// Remove Review Action
+export const removeReview = (reviewId) => ({
+    type: REMOVE_REVIEW,
+    reviewId
+})
+
+
+// THUNKS
+
+// load Reviews thunk
 export const loadReviews = () => async dispatch => {
-    let res = await fetch('/api/reviews')
+    let res = await fetch('/api/reviews') // probably needs to be /api/products/:productId/reviews
     if(res.ok) {
         res = await res.json()
         dispatch(load(res))
     }
 }
+
+// add Review thunk
+export const createReview = (review) => async dispatch => {
+    let res = await fetch('/api/reviews', { // probably change to /api/products/{review.productId}/reviews
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(review)
+    });
+
+    if(res.ok) {
+        const newReview = await res.json();
+        dispatch(addReview(newReview));
+        return newReview;
+    }
+}
+
+// edit Review thunk
+export const updateReview = (review) => async dispatch => {
+    const res = await fetch(`/api/products/${review.productId}/reviews`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(review)
+    });
+
+    if(res.ok) {
+        const updatedReview = await res.json();
+        dispatch(editReview(updatedReview));
+        return updatedReview;
+    }
+}
+
+// delete Review thunk
+export const deleteReview = (reviewId) => async dispatch => {
+    const res = await fetch(`/api/reviews/${reviewId}`, { // may have to adjust route
+        method: "DELETE"
+    });
+
+    if(res.ok) {
+        const review = await res.json()
+        dispatch(removeReview(review))
+    }
+}
+
 
 // STATE
 const initialState = {}
@@ -24,6 +95,18 @@ const reviews = (state=initialState, action) => {
     switch(action.type) {
         case LOAD_REVIEWS:
             return {...state, ...action.reviews}
+
+        case ADD_REVIEW:
+            return {...state, [action.review.id]: action.review}
+
+        case EDIT_REVIEW:
+            const updateRev = action.review.updateReview;
+            return {...state, [updateRev.id]: updateRev};
+
+        case REMOVE_REVIEW:
+            const deleteState = {...state};
+            delete deleteState[action.reviewId.id]
+            return deleteState;
         default:
             return state
     }
