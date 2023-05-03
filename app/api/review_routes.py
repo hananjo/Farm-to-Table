ac8f6c50e2efc697f0b71ac3f4eaf7c23a33c733
david <david.chaotic.chu@gmail.com>
2023-05-02 22:56:15 -0400

    add review store in react app

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import Review
+
 
 review_routes = Blueprint('reviews', __name__)
 
 # REMINDERS
 # in seeds/__init__.py add an import for review routes and add app.register_blueprint() for review_routes
+# in Review model, include UserMixin, and def to_dict(self): -> return {'id': self.id, 'review':self.review, 'user_id': self.user_id, etc}
 
 @review_routes.route('/', methods=["GET"])
 def reviews():
-    pass
+    return {review.id: review.to_dict() for review in Review.query.all}
