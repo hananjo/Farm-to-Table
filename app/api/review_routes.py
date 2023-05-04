f47009329f3991821607c2553c4ae785f65d1f6d
david <david.chaotic.chu@gmail.com>
2023-05-03 23:22:02 -0400

    add_review route, removed edit review in store

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import Review
+from app.models import Review, db
 
 
 review_routes = Blueprint('reviews', __name__)
@@ -12,3 +12,29 @@ review_routes = Blueprint('reviews', __name__)
 @review_routes.route('/', methods=["GET"])
 def reviews():
     return {review.id: review.to_dict() for review in Review.query.all}
+
+@review_routes.route('/products/<id>/reviews', methods=["POST"])
+@login_required
+def add_review():
+    user_id = current_user.id
+
+    data = request.json
+    review_text = data.get('review', '')
+    rating = data.get('rating', '')
+
+    product = Product.query.get(id)
+
+    if not product:
+        return jsonify(error='Product not found', 404)
+
+    review = Review(
+        review=review_text,
+        rating=rating,
+        user_id=user_id,
+        product_id=product.id
+    )
+
+    db.session.add(review)
+    db.session.commit()
+
+    return jsonify(review.to_dict())
