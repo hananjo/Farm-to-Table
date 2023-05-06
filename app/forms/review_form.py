84e17c46d42928483573c826daaf1208954e1ef0
david <david.chaotic.chu@gmail.com>
2023-05-06 13:39:01 -0400

    commented out review form L8-9 and image form L8

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 class ReviewForm(FlaskForm):
     review = StringField("Review", validators=[DataRequired()])
     rating = IntegerField("Rating", validators=[DataRequired()])
-    user_id = IntegerField("User_id", validators=[DataRequired()])
-    product_id = IntegerField("Product_id", validators=[DataRequired()])
+    # user_id = IntegerField("User_id", validators=[DataRequired()])
+    # product_id = IntegerField("Product_id", validators=[DataRequired()])
     submit = SubmitField("Submit")
