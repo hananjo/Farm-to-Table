57c36f7f77137300bb727da5c8b42d02e5bf22c6
Hanan Jomaa <hanan91jomaa@gmail.com>
2023-05-10 12:49:41 -0700

    landing page styling

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-			<li>
-				<NavLink exact to="/cart">Cart</NavLink>
-			</li>
-		</ul>
-	);
+  return (
+    <ul>
+      <div className="nav-bar-container">
+        <div className="home-button">
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+        </div>
+        <div className="nav-right">
+          {isLoaded && (
+            <li>
+              <ProfileButton user={sessionUser} />
+            </li>
+          )}
+
+          <li>
+            <NavLink exact to="/cart">
+              Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/products/new">
+              Post your product
+            </NavLink>
+          </li>
+        </div>
+      </div>
+    </ul>
+  );
 }
 
 export default Navigation;
--- react-app/src/components/Products/index.js
+++ react-app/src/components/Products/index.js
@@ -2,11 +2,9 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // import { NavLink } from "react-router-dom";
 import React from "react";
-import {
-  getAllProducts,
-  getProductDetails,
-  // getProductImages,
-} from "../../store/product";
+import { getAllProducts } from "../../store/product";
+import "./LandingPage.css";
+import { NavLink } from "react-router-dom";
 const Products = () => {
   const dispatch = useDispatch();
 
@@ -26,25 +24,96 @@ const Products = () => {
 
   return (
     <div>
-      <h1>DHT</h1>
-      <h2>Groceries delivered right to your door!</h2>
-      <div>
+      <div className="category-container">
+        <div className="categories-title">
+          <h2>Farm-to-Table. Groceries delivered right to your door!</h2>
+        </div>
+        <div className="categories-and-names">
+          <div className="category">
+            <img
+              src={
+                "https://res.cloudinary.com/dwphwqyrn/image/upload/v1683739735/Fruits_xat3pu.jpg"
+              }
+              style={{
+                clipPath: "circle(38%)",
+                width: "310px",
+              }}
+              alt="image1"
+              className="image-categories"
+            />
+            <div className="category-names">
+              <p>Fruits</p>
+            </div>
+          </div>
+          <div className="category">
+            <img
+              src={
+                "https://res.cloudinary.com/dwphwqyrn/image/upload/v1683739735/vegetables_2_qhyckt.jpg"
+              }
+              style={{ clipPath: "circle(38%)" }}
+              alt="image2"
+              className="image-categories"
+            />
+            <div className="category-names">
+              <p>Vegetables</p>
+            </div>
+          </div>
+          <div className="category">
+            <div className="image-meat">
+              <img
+                src={
+                  "https://res.cloudinary.com/dwphwqyrn/image/upload/v1683739735/meat_products_jfojgq.jpg"
+                }
+                style={{ clipPath: "circle(30%)", width: "400px" }}
+                alt="image3"
+                className="image-categories"
+              />
+            </div>
+            <div className="category-name-meat">
+              <p>Meat</p>
+            </div>
+          </div>
+          <div className="category">
+            <img
+              src={
+                "https://res.cloudinary.com/dwphwqyrn/image/upload/v1683739735/dairy_g1nr3q.jpg"
+              }
+              style={{ clipPath: "circle(38%)" }}
+              alt="image4"
+              className="image-categories"
+            />
+            <div className="category-names">
+              <p>Dairy</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="product-and-pricing">
         {products?.map((product) => {
           return (
             <div>
-              <div>{product.price}</div>
-              <div>
-                <img
-                  src={product.images[0].image_url}
-                  style={{
-                    width: "200px",
-                    // marginLeft: "400px",
-                    marginRight: "30px",
-                    marginBottom: "30px",
-                    marginTop: "30px",
-                  }}
-                />
-              </div>
+              <NavLink
+                key={product.id}
+                to={`/products/${product.id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <div className="product-image-listing">
+                  <img
+                    src={product?.images[0]?.image_url}
+                    style={{
+                      // width: "200px",
+                      // marginLeft: "400px",
+                      marginRight: "30px",
+                      marginBottom: "30px",
+                      marginTop: "30px",
+                    }}
+                    className="product-images"
+                  />
+                </div>
+                <div className="product-pricing-images">
+                  ${product.price.toFixed(2)}
+                </div>
+              </NavLink>
             </div>
           );
         })}
