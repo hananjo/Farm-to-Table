3528df739af748d38499f15645e92a529fde4f4b
Laila <lailadenman@gmail.com>
2023-08-11 16:05:51 -0700

    total updated

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { getCart, deleteFromCart } from "../../store/cart";
+import { getCart, deleteFromCart, checkoutCart } from "../../store/cart";
 import { useEffect, useState } from "react";
 import "./cart.css"
 import CartQtyForm from "../CartQtyForm/"
@@ -12,6 +12,7 @@ function Cart() {
     const [display, setDisplay] = useState("")
     const [save, setSave] = useState("hidden")
     const [showModal, setShowModal] = useState(false);
+    // const [cartTotal, setCartTotal] = useState(0)
     const user = useSelector(state => state.session?.user?.id)
     const sessionUser = useSelector(state => state.session?.user)
 
@@ -19,13 +20,17 @@ function Cart() {
     useEffect(() => {
         dispatch(getCart(user));
         setIsLoaded(true)
-    }, [dispatch, user])
+    }, [dispatch])
 
+    const cart = useSelector(state => state?.cart)
 
-
-    const cart = useSelector(state => state.cart)
+    console.log(cart && cart, "cart");
 
     const cartArr = Object.values(cart)
+
+    cartArr.pop()
+
+    const total = cart?.total
 
     const onDelete = (e) => {
         dispatch(deleteFromCart(user, e.target.id))
@@ -40,9 +45,29 @@ function Cart() {
         openModal();
     }
 
+    const handleCheckout = () => {
+        // console.log("checkout clicked");
+        // dispatch(checkoutCart(user))
+        dispatch(getCart(user))
+    }
+
+    // useEffect(() => {
+    //     dispatch(getCart(user))
+    // }, [dispatch, handleCheckout])
+
+    // let cartTotal = 0;
+
+    // const handleTotal = (price) => {
+    //     setCartTotal(cartTotal + price)
+    // }
+
     const cartContent = () => {
         if (cartArr.length > 0) {
             return (isLoaded && cartArr && cartArr?.map(prod => {
+                // setCartTotal(cartTotal + prod?.product?.price.toFixed(2))
+                // cartTotal = (cartTotal + Number(prod?.product?.price.toFixed(2)))
+                // const prodPrice = prod?.product?.price.toFixed(2)
+                // handleTotal(prodPrice)
                 return (
                     <div className="cart-prod" key={prod?.id}>
                         <div className="product-info">
@@ -84,16 +109,22 @@ function Cart() {
     }
 
     return (
-        <div>
-            <div className="cart-page-banner">
+        <div className="cart-page">
+            <div className="cart-info-container">
+                <div className="cart-page-banner">
 
+                </div>
+                <div className="title-container">
+                    <h1>Your Shopping Cart </h1>
+                </div>
+                {sessionUser && user ? (<div className="cart-container-item-list">
+                    {cartContent()}
+                </div>) : (<h1>Please Log in to View Your Cart</h1>)}
             </div>
-            <div className="title-container">
-                <h1>Your Shopping Cart </h1>
+            <div className="checkout-info-container">
+                Total: {cart?.total ? cart?.total : 0}
+                <button onClick={handleCheckout}>Checkout</button>
             </div>
-            {sessionUser && user ? (<div className="cart-container-item-list">
-                {cartContent()}
-            </div>) : (<h1>Please Log in to View Your Cart</h1>)}
         </div>
     )
 }
--- react-app/src/store/cart.js
+++ react-app/src/store/cart.js
@@ -6,15 +6,25 @@ const LOAD_CART = 'user/LOAD_CART'
 const UPDATE_CART = 'user/UPDATE_CART'
 // Delete a product from the cart
 const DELETE_CART = 'user/DELETE_CART'
+// Gets the total of the cart
+const GET_TOTAL = 'user/GET_TOTAL'
+// Clears the cart
+const CLEAR_CART = 'user/CLEAR_CART'
 
 const addCartProd = (cartRel) => ({
     type: ADD_CART,
     cartRel
 })
 
-const loadCart = (list) => ({
+const loadTotal = (total) => ({
+    type: GET_TOTAL,
+    total
+})
+
+const loadCart = (list, total) => ({
     type: LOAD_CART,
-    list
+    list,
+    total
 });
 
 const deleteCartProd = (id) => ({
@@ -27,10 +37,14 @@ const updateCartQty = (cartRel) => ({
     cartRel
 })
 
+const clearCart = () => ({
+    type: CLEAR_CART
+})
+
 const initialState = {};
 
 export const addProdToCart = (cartRel) => async (dispatch) => {
-    const {prodId, user, qty} = cartRel
+    const { prodId, user, qty } = cartRel
 
     const res = await fetch(`/api/users/${user}/cart/${prodId}`, {
         method: 'POST',
@@ -54,13 +68,47 @@ export const getCart = (id) => async (dispatch) => {
 
     if (res.ok) {
         const products = await res.json()
+        console.log(products);
 
-        dispatch(loadCart(products))
+        let total = 0;
+
+        products.forEach((prod) => {
+            total = total + (Number(prod.product.price) * prod.quantity)
+            // console.log("new cart total is ", total);
+        })
+
+        total = total.toFixed(2)
+
+        console.log("final cart total is ", total);
+
+        dispatch(loadCart(products, total))
     }
 }
 
+export const getTotal = (id) => async (dispatch) => {
+    const res = await fetch(`/api/users/${id}/cart`)
+
+    if (res.ok) {
+        const products = await res.json()
+
+        let total = 0;
+
+        products.forEach((prod) => {
+            total = total + (Number(prod.product.price) * prod.quantity)
+        })
+
+        total = total.toFixed(2)
+
+        console.log("cart total is", total);
+
+        dispatch(loadTotal(total))
+    }
+
+    console.log("cart total not found");
+}
+
 export const updateQty = (cartRel) => async (dispatch) => {
-    const {prodId, user, qty} = cartRel
+    const { prodId, user, qty } = cartRel
 
     const res = await fetch(`/api/users/${user}/cart/${prodId}`, {
         method: 'PUT',
@@ -75,6 +123,8 @@ export const updateQty = (cartRel) => async (dispatch) => {
     if (res.ok) {
         const updatedCartRel = await res.json()
 
+        dispatch(getTotal(user))
+
         dispatch(updateCartQty(updatedCartRel))
     }
 }
@@ -86,9 +136,30 @@ export const deleteFromCart = (userId, prodId) => async (dispatch) => {
 
     if (res.ok) {
         const cartRel = await res.json()
+
+        dispatch(getTotal(userId))
+
         dispatch(deleteCartProd(cartRel.id))
     }
 }
+
+export const checkoutCart = (userId) => async (dispatch) => {
+    const res = await fetch(`/api/users/${userId}/cart/checkout`, {
+        method: 'DELETE'
+    })
+
+    console.log("checkout thunk clicked");
+
+    if (res.ok) {
+        const cartRel = await res.json()
+
+        console.log("checkout thunk completed");
+
+        dispatch(clearCart())
+
+    }
+}
+
 
 
 
@@ -101,6 +172,8 @@ export default function cartReducer(state = initialState, action) {
             action.list.forEach((cartRel) => {
                 newState[cartRel.id] = cartRel;
             });
+
+            newState["total"] = Number(action.total).toFixed(2)
 
             return { ...newState }
         case ADD_CART:
@@ -119,6 +192,16 @@ export default function cartReducer(state = initialState, action) {
             newState = { ...state }
 
             delete newState[action.id]
+
+            return newState
+        case GET_TOTAL:
+            newState = { ...state }
+
+            newState.total = action.total
+
+            return newState
+        case CLEAR_CART:
+            newState = { ...initialState }
 
             return newState
         default:
