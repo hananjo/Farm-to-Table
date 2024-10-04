65edb55990c4969a609dd973168b77a0e2c2c855
Hanan <hanan91jomaa@gmail.com>
2024-10-04 11:06:09 -0700

    changed description limit in versions

--- migrations/versions/20230501_233041_.py
+++ migrations/versions/20230501_233041_.py
@@ -23,7 +23,7 @@ def upgrade():
     op.create_table('products',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=50), nullable=False),
-    sa.Column('description', sa.String(length=50), nullable=True),
+    sa.Column('description', sa.String(length=250), nullable=True),
     sa.Column('price', sa.Float(), nullable=False),
     sa.Column('type', sa.String(), nullable=False),
     sa.Column('owner_id', sa.Integer(), nullable=True),
